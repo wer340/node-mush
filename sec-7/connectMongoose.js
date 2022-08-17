ae569b5f4bab03bd02e7516ff03823cc57982081
hamed.sh <hsee1372@gmail.com>
2022-08-17 22:26:32 +0430

    mongodb save data

--- sec-7/connectMongoose.js
+++ sec-7/connectMongoose.js
@@ -1,5 +1,33 @@
 const mongoose=require("mongoose");
-
+//address host write in config file in here is talked  mongo db  
 mongoose.connect("mongodb://localhost/playground").//automatically playground db is made
 then(()=>console.log("connecting mongoose successfully ..."))
 .catch((err)=>console.error("error has occurred ... ",err))
+
+const schemaData=new mongoose.Schema({ //! Schema class pascal case no schima
+    name:String,
+    author:String, //! capital case S
+    tags:[ String ],
+    date:{type:Date,deafult:Date.now},
+    isPublished:Boolean // ! capital case Boolean
+});
+// *classes object
+//human    john
+
+const Data=mongoose.model("actress",schemaData) // *pascal case  for class
+
+
+async function dataSendToDb(){
+    const data=new Data({
+        name:"emilia",
+        author:"clarcke",
+        tags:["actress","movie"],
+        isPublished:true
+    }
+    
+    )
+    const result=await data.save() // *is synchros  operation 
+    console.log(result)
+}
+
+dataSendToDb()
